7dca301eb6e359079637d15a3faadb11244e7a07
Adarsh Rawal <adarshrawal321@gmail.com>
2024-04-29 14:59:14 +0545

    changed navbar using bootstrap

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,30 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+<>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Hamro Bazar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+      <button class="btn btn-outline-success" type="submit">Register</button>
+        <button class="btn btn-outline-success" type="submit">Login</button>
+      </form>
     </div>
+  </div>
+</nav>
+</>
   )
 }
 
