c16e97397c57cdfbe7aa0786ba2890ecc359a7db
Adarsh Rawal <adarshrawal321@gmail.com>
2024-05-13 15:51:02 +0545

    easy implementation of code

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  //use State
+  const [email,setEmail]=useState('')
+  const [password,setPassword]=useState('')
   return (
-    <div>
+    <div className='m-3'>
       <h1>
-        Login Page
+        Login into your account
       </h1>
+      <form className='w-50'>
+        <label >Enter your Email:{email}</label>
+        <input onChange={(e)=>setEmail(e.target.value)} type="Email" className='form-control' placeholder='Email'  />
+
+        <label >Enter your password</label>
+        <input onChange={(e)=>setPassword(e.target.value)} type="password" className='form-control' placeholder='Password' />
+        
+        <button className='btn btn-dark mt-2 w-100'>Create Account</button>
+
+      </form>
     </div>
   )
 }
