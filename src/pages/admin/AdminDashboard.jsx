ee7335d7e6956738fa559e650d530e4df31bb5b6
Adarsh Rawal <adarshrawal321@gmail.com>
2024-05-20 15:47:08 +0545

    made the input category

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -1,73 +1,136 @@
-import React from 'react'
+import React,{useState} from "react";
 
 const AdminDashboard = () => {
+    //Making a state for product 
+    const[productName,SetProductName]=useState("")
+    const[productPrice,SetProductPrice]=useState("")
+    const[productCategory,SetProductCategory]=useState("")
+    const[productDescription,SetProductDescription]=useState("")
+    const[productImage,SetProductImage]=useState("")
   return (
     <>
+      <div className="container">
+        <div className="d-flex justify-content-between mt-3">
+          <h2>Admin Dashboard</h2>
+          <button
+            type="button"
+            class="btn btn-primary"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+          >
+            Add product
+          </button>
 
-    <div className='container'>
-    <div className='d-flex justify-content-between mt-3'>
-        <h2>
-            Admin Dashboard
-        </h2>
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Add product
-</button>
+          <div
+            class="modal fade"
+            id="exampleModal"
+            tabindex="-1"
+            aria-labelledby="exampleModalLabel"
+            aria-hidden="true"
+          >
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h1 class="modal-title fs-5" id="exampleModalLabel">
+                    Create a New Product!!!
+                  </h1>
+                  <button
+                    type="button"
+                    class="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  ></button>
+                </div>
+                <div class="modal-body">
+                  <form action="">
+                  onChange={(e)=>SetProductName(e.target.value)}
+                    <label>Product Name</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Enter Product Name"
+                    />
 
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-    </div>
-    <div class="container mt-3">
-    </div>
-    <table className='table'>
-        <thead className='table-dark'>
-            <tr>
-                <th>Product Image</th>
-                <th>Product Name</th>
-                <th>Price</th>
-                <th>Category</th>
-                <th>Description</th> 
-                <th>Action </th>
-            </tr>
-        </thead>
-           <tbody>
-            <tr className='table-light'>
-                <td>
-                    <img height={'100px'} width={'160px'} src="https://th.bing.com/th/id/OIP.LhgZOlbBt3HF8BSN2dbokQHaEf?rs=1&pid=ImgDetMain" alt="" />
-                </td>
-                <td>Iron Man</td>
-                <td>NPR 1000000</td>
-                <td>Action Movies</td>
-                <td>Marvel Movies </td>
-                <td>
-                    <div className='btn-group 'role='group'>
-                        <button className='btn btn-success'>Edit</button>
-                        <button className='btn btn-danger'>Delete</button>
+                    <label>Product Price</label>
+                    <input
+                    onChange={(e)=>SetProductPrice(e.target.value)}
+                      type="number"
+                      className="form-control"
+                      placeholder="Enter Product Price"
+                    />
+
+                    <div className="mt-2">
+                    
+                      <label>Select Category</label>
+                      <select className="form-control"
+                      onChange={(e)=>SetProductCategory(e.target.value)}
+                      >
+                        <option value="action">Action Movies</option>
+                        <option value="love">Love Movies</option>
+                        <option value="romance">Romance Movies</option>
+                      </select>
                     </div>
-                </td>
+
+                    <label className="mt-2">Type Product descriprion</label>
+                    <textarea id="" className="form-control" onChange={(e)=>SetProductDescription(e.target.value)}></textarea>
+                    <label className="mt-2">Product Image</label>
+                    <input type="file" className="form-control" />
+                  </form>
+                </div>
+                <div class="modal-footer">
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    data-bs-dismiss="modal"
+                  >
+                    Close
+                  </button>
+                  <button type="button" class="btn btn-primary">
+                    Save changes
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="container mt-3"></div>
+        <table className="table">
+          <thead className="table-dark">
+            <tr>
+              <th>Product Image</th>
+              <th>Product Name</th>
+              <th>Price</th>
+              <th>Category</th>
+              <th>Description</th>
+              <th>Action </th>
             </tr>
-           </tbody>
-
-            
-    </table>
-</div>
-    
+          </thead>
+          <tbody>
+            <tr className="table-light">
+              <td>
+                <img
+                  height={"100px"}
+                  width={"160px"}
+                  src="https://th.bing.com/th/id/OIP.LhgZOlbBt3HF8BSN2dbokQHaEf?rs=1&pid=ImgDetMain"
+                  alt=""
+                />
+              </td>
+              <td>Iron Man</td>
+              <td>NPR 1000000</td>
+              <td>Action Movies</td>
+              <td>Marvel Movies </td>
+              <td>
+                <div className="btn-group " role="group">
+                  <button className="btn btn-success">Edit</button>
+                  <button className="btn btn-danger">Delete</button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default AdminDashboard
+export default AdminDashboard;
